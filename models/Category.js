eac3244e97c9093c194b93d5ffda5191b385105f
CJ Soriano <soriano.christianjose.m@gmail.com>
2024-09-29 18:07:37 +0800

    typo category_url => image_url

--- models/Category.js
+++ models/Category.js
@@ -1,8 +1,8 @@
 class Category {
-    constructor(category_id, category_name, category_url){
+    constructor(category_id, category_name, image_url){
         this.category_id = category_id;
         this.category_name = category_name;
-        this.category_url = category_url;
+        this.image_url = image_url;
     }
 }
 module.exports = Category;
